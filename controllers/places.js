a57e7223387c71c3ad6f908b24479ac4a8a744df
matt rodman <mrodman89@gmail.com>
2022-10-18 19:20:39 -0500

    made changes to places.js again

--- controllers/places.js
+++ controllers/places.js
@@ -1,20 +1,11 @@
-require('dotenv').config()
-const { Router } = require('express')
-const express = require('express')
-const app = express()
-Router.get('/', (req, res) => {
+const router = require('express').Router()
+
+router.get('/', (req, res) => {
     res.send('GET /places')
 })
+
 module.exports = router
-app.use('/places', require('./controllers/places'))
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
 
-app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
-})
 
-app.listen(process.env.PORT)
 
