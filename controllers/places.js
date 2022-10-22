0812d4473d3d315a6ca969e8a520dfbf1b478725
matt rodman <mrodman89@gmail.com>
2022-10-22 13:09:01 -0500

    Udpated forms

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 //GET /places
 router.get(`/`, (req, res) => {
@@ -17,9 +18,23 @@ router.get(`/`, (req, res) => {
       }];      
     res.render(`places`,{ places })
 })
-router.post(`/`, (req, res) => {
-    res.render(`text`)
-})
+router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Osage City'
+    }
+    if (!req.body.state) {
+      req.body.state = 'Kansas'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
+  
+  
 router.get('/new', (req, res) => {
     res.render('places/new')
   })
